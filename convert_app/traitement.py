4443c70c08c1216e4bcb395ad4c7636e60355d42
CameliaB2 <camelia.bennamane@gmail.com>
2022-07-05 11:37:35 +0200

    adding the creation of delta values file

--- convert_app/traitement.py
+++ convert_app/traitement.py
@@ -45,3 +45,23 @@ def calculate_average_all_data(aX, aY, aZ, gX, gY, gZ, _range):
 		#aver_datas = np.concatenate(aver_acc_x, aver_acc_y, aver_acc_z, aver_gyr_x, aver_gyr_y, aver_gyr_z)
 	"""
 	return aver_data
+
+
+def calculate_delta(_array):
+	delta = []
+	for i in range(len(_array)-1):
+		delta.append(_array[i+1] -  _array[i])
+	return delta
+
+def calculate_delta_all_data(aX, aY, aZ, gX, gY, gZ):
+	delta_data = [[], [], [], [], [], []]
+	delta_data[0] = calculate_delta(aX)
+	delta_data[1] = calculate_delta(aY)
+	delta_data[2] = calculate_delta(aZ)
+
+	delta_data[3] = calculate_delta(gX)
+	delta_data[4] = calculate_delta(gY)
+	delta_data[5] = calculate_delta(gZ)	
+	sleep(0.5)
+	
+	return delta_data	
